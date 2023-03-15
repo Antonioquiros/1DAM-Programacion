041d17c9b12e66090d3c12ae05f64b1a259e4f53
Antonio Quirós <115268470+Antonioquiros@users.noreply.github.com>
2023-03-15 16:36:21 +0100

    Add files via upload

---
+++ Ejercicios_T5/Ejercicio11/CabreoException.java
@@ -0,0 +1,12 @@
+package EquipoFutbol;
+
+/**
+ *
+ * @author Antonio Quirós García <aquigar1604@g.educaand.es>
+ */
+public class CabreoException extends Exception{
+    public CabreoException(String m){
+    super(m);
+    }
+    
+}
---
+++ Ejercicios_T5/Ejercicio11/Empleado.java
@@ -0,0 +1,32 @@
+package EquipoFutbol;
+
+/**
+ *
+ * @author Antonio Quirós García <aquigar1604@g.educaand.es>
+ */
+public class Empleado {
+    private String nombre;
+    private double sueldo;
+    private double dinero;
+    
+    public Empleado(String n, double s){
+    this.nombre = n;
+    this.sueldo = s;
+    this.dinero = 0;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public double getDinero() {
+        return dinero;
+    }
+    public void cobrar(double cantidad)throws CabreoException{
+    this.dinero += cantidad;
+    if(cantidad < this.sueldo){
+    CabreoException e = new CabreoException("Al empleado "+this.nombre+" no se le han pagado "+(this.sueldo-cantidad)+" euros");
+    throw e;
+    }
+    }
+}
